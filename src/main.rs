1e39a036afa54b8524373f1e06f740e4a13c0075
Lars Kue <lars@kuehmichel.de>
2020-03-27 15:22:30 +0100

    Texture Draw Setup (almost)

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,41 @@
 
+// extern crate piston;
 extern crate piston_window;
+
+
+mod colors;
+
 
 use piston_window::*;
 
 fn main() {
-
-    let white = [1.0; 4];
-    // let black = [0.0, 0.0, 0.0, 1.0];
-    let red = [1.0, 0.0, 0.0, 1.0];
+    let opengl  = OpenGL::V3_2;
+    // let mut gl  = GlGraphics::new(opengl);
 
     let mut window : PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
         .exit_on_esc(true)
         .build()
         .expect("Failed to create window.");
 
+
+    let texture_settings = TextureSettings::new();
+
+    //Create the image object and attach a square Rectangle object inside.
+    // let img   = image::Image::new().rect(square(0.0, 0.0, 200.0));
+    //A texture to use with the image
+    // let texture = Texture::from_path(Path::new("example.png"), &texture_settings).unwrap();
+
+    let draw_state = DrawState::default();
+
+    //Main loop
     while let Some(event) = window.next() {
-        window.draw_2d(&event, |context, graphics, _device| {
-            clear(white, graphics);
-            rectangle(red, [0.0, 0.0, 100.0, 100.0], context.transform, graphics);
+        window.draw_2d(&event, |mut context, window_graphics, _device| {
+
+            let texture = Texture::from_path(&mut context, std::path::Path::new("example.png"), Flip::None, &texture_settings).unwrap();
+            clear(colors::WHITE, window_graphics);
+            rectangle(colors::RED, [0.0, 0.0, 100.0, 100.0], context.transform, window_graphics);
+
+            image(&texture, context.transform, window_graphics);
         });
     }
 }
